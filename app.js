437154394bc79cbc401823e508dedab4fa72789f
Chester Alejandro <chesterm.alejandro@gmail.com>
2021-08-21 12:53:58 +0800

    minor changes

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ mainLinks.forEach(mainLink => mainLink.addEventListener('click', () => {
 /** THIS IS FOR THE TYPEWRITTER EFFECT **/
 function typewritter(){
     const cursor = document.querySelector('.banner__cursor');
-    const title = ['Programmer.', 'Web Developer.', 'Web Designer.'];
+    const title = ['Programmer.', 'Web Developer.', 'Web Designer.', 'Backend Developer.'];
     let word = ''; 
     let setWord = '';
     let index = 0;
@@ -106,42 +106,6 @@ function typewritter(){
     }; 
     type();
 } typewritter();
-
-/** ANY EFFECTS THAT IS CONNECTED TO SCROLL EVENT **/
-const mainNavigation = document.querySelector('.navigation');
-window.addEventListener("scroll", () => {
-    let scroll = window.pageYOffset;
-    //GIVES BACKGROUND COLOR TO THE NAVIGATION WHEN YOU SCROLL A LITTLE
-    if (scroll < 50) {
-        mainNavigation.style.backgroundColor = "transparent";
-        //mainNavigation.style.backdropFilter = "blur(0)";
-    }
-         else {
-        mainNavigation.style.backgroundColor = "rgba(29, 60, 69, 0.9)";
-        //mainNavigation.style.backdropFilter = "blur(0.4rem)";
-    }
-    let screenSize = window.innerHeight / 1.1;
-    //ICONS YOU WANT TO FADE IN WHEN YOU SCROLL
-    const contactLink = document.querySelector('.contact__link');
-    const icons = document.querySelectorAll('.contact__icon');
-    let contactLinkPosition = contactLink.getBoundingClientRect().top;
-    if(contactLinkPosition < screenSize-50) {
-        icons.forEach((icon, index) => {
-            icon.style.transition = `
-                transform 1000ms ${index - 0.3}s ease-out,
-                opacity 500ms ${index - 0.3}s ease-out`;
-            icon.style.transform = "translate(0%, 0%)";
-            icon.style.opacity = "1";
-        }); // end forEach
-    } else {
-        icons.forEach((icon, index) => {
-            icon.style.transition = "none";
-            if(window.innerWidth <= 768) icon.style.transform = "translate(-200%, 0%)";
-            else icon.style.transform = "translate(0%, -200%)";
-            icon.style.opacity = "0";
-        }); // end forEach
-    } // end else
-})
 
 
 /** THIS IS FOR THE CAROUSEL **/
@@ -186,6 +150,42 @@ setInterval(() => {
     }
 }, 1000);
 
+
+/** ANY EFFECTS THAT IS CONNECTED TO SCROLL EVENT **/
+const mainNavigation = document.querySelector('.navigation');
+window.addEventListener("scroll", () => {
+    let scroll = window.pageYOffset;
+    //GIVES BACKGROUND COLOR TO THE NAVIGATION WHEN YOU SCROLL A LITTLE
+    if (scroll < 50) {
+        mainNavigation.style.backgroundColor = "transparent";
+    }
+         else {
+        mainNavigation.style.backgroundColor = "rgba(29, 60, 69, 0.9)";
+    }
+    let screenSize = window.innerHeight / 1.1;
+    //ICONS YOU WANT TO FADE IN WHEN YOU SCROLL
+    const contactLink = document.querySelector('.contact__link');
+    const icons = document.querySelectorAll('.contact__icon');
+    let contactLinkPosition = contactLink.getBoundingClientRect().top;
+    if(contactLinkPosition < screenSize) {
+        icons.forEach((icon, index) => {
+            icon.style.transition = `
+                transform 1000ms ${index - 0.3}s ease-out,
+                opacity 500ms ${index - 0.3}s ease-out`;
+            icon.style.transform = "translate(0%, 0%)";
+            icon.style.opacity = "1";
+        }); // end forEach
+    } else {
+        icons.forEach((icon, index) => {
+            icon.style.transition = "none";
+            if(window.innerWidth <= 768) icon.style.transform = "translate(-200%, 0%)";
+            else icon.style.transform = "translate(0%, -200%)";
+            icon.style.opacity = "0";
+        }); // end forEach
+    } // end else
+})
+
+
 /** THESE VARIABLES ARE FOR THE MODAL **/
 const modal = document.querySelector('.modal');
 const modalBox = document.querySelector('.modal__box');
@@ -204,16 +204,17 @@ submit.addEventListener('click', e => {
     modal.style.visibility = "visible";
     modalBox.style.transition = "transform 300ms 30ms ease";
     modalBox.style.transform = "scale(1)";
+    modal.style.backdropFilter = "blur(12px)";
     if(nameField.value == '' && emailField.value == '' && messageField.value == '') {
-        modalMessage.innerText = 'Please say something to me.';
+        modalMessage.innerHTML = '<h3 class="modal__message">Please say something to me.</h3>';
     } else if (nameField.value == '') {
-        modalMessage.innerText = 'What is your name?';
+        modalMessage.innerText = '<h3 class="modal__message">What is your name?</h3>';
     } else if (emailField.value == '') {
-        modalMessage.innerText = 'Please provide you email address.';
+        modalMessage.innerHTML = '<h3 class="modal__message">Please provide your email address.</h3>';
     } else if (messageField.value == '') {
-        modalMessage.innerText = `You forgot to say your message ${nameField.value}.`;
+        modalMessage.innerHTML = `<h3 class="modal__message">You forgot to say your message <span class="modal__name">${nameField.value}</span>.</h3>`;
     }else {
-        modalMessage.innerText = `Thank you ${nameField.value}, your message was sent to Chester Alejandro.`;
+        modalMessage.innerHTML = `<h3 class="modal__message">Thank you <span class="modal__name">${nameField.value}</span>, your message is sent to <span class="modal__name">Chester Alejandro</span>.</h3>`;
         nameField.value = '';
         emailField.value = '';
         messageField.value = '';
